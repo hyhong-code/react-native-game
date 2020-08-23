e7ba86409c3107b01021b293d159372680b2bdfb
Haiyang Hong <hong961127@gmail.com>
2020-08-22 17:18:29 -0700

    Make Card reusable

---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Card = ({ children, style }) => {
+  return <View style={{ ...styles.card, ...style }}>{children}</View>;
+};
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: "#fff",
+    elevation: 5,
+    padding: 20,
+    borderRadius: 10,
+  },
+});
+
+export default Card;
--- src/screens/StartGameScreen.js
+++ src/screens/StartGameScreen.js
@@ -1,8 +1,44 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+import Card from "../components/Card";
 
 const StartGameScreen = () => {
-  return;
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a new game!</Text>
+      <Card style={styles.form}>
+        <Text>Select a number!</Text>
+        <TextInput />
+        <View style={styles.actions}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </Card>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  form: {
+    alignItems: "center",
+    width: 300,
+    maxWidth: "80%", // If device too small, constraint to 80%
+  },
+  actions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+});
 
 export default StartGameScreen;
